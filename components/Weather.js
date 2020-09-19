90d8cf1f155211e8ad04ce771bede176d4b257b4
kla004 <fiw56789@gmail.com>
2020-09-19 11:14:03 +0700

    Add useEffect+zipcode

--- components/Weather.js
+++ components/Weather.js
@@ -2,7 +2,7 @@ import React, { useState,useEffect } from 'react';
 import { View, Text, ImageBackground, StyleSheet } from 'react-native';
 import Forecast from './Forecast';
 
-const apiKey = 'YOUR_API_KEY'
+const apiKey = '6cecb9dd2e369d7e9b5d62bc682150d4'
 export default function Weather(props) {
     const [forecastInfo, setForecastInfo] = useState({
         main: 'main',
@@ -12,10 +12,11 @@ export default function Weather(props) {
     useEffect(() => {
         console.log(`fetching data with zipCode = ${props.zipCode}`)
         if (props.zipCode) {              
-   
+
    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=${apiKey}`)
                    .then((response) => response.json())
                    .then((json) => {
+                       console.log(json.weather[0])
                        setForecastInfo({
                            main: json.weather[0].main,
                            description: json.weather[0].description,
